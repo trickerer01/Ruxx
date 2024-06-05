ea27206241d175406dc2035440599845e28bb859
trickerer01 <onlysuffering+1@gmail.com>
2024-06-05 12:33:15 +0700

    Rev 572: Fix test names order

--- src/app_unittests.py
+++ src/app_unittests.py
@@ -27,31 +27,31 @@ __all__ = ('run_all_tests',)
 RUN_CONN_TESTS = 0
 CUR_PATH = normalize_path(path.abspath(curdir))
 
-args_argparse_str1 = (
+args_argparse_str01 = (
     'sfw asd ned -nds -proxr '
     '-timeout 13 -retries 56 -dmode 0 -skip_img -skip_vid -lowres -noproxy -proxynodown -prefix -dump_tags -dump_sources -append_info'
 )
-args_argparse_str2_base = (
+args_argparse_str02_base = (
     'sfw asd ned -nds -proxt '
     '-threads 8 -proxy http://8.8.8.8:65333 '
     '-headers {"name1":"value1"} -cookies {"name2":"value2"} '
     '-path ' + CUR_PATH
 )
-args_argparse_str2 = args_argparse_str2_base + ' -mindate 31-12-1950 -maxdate 01-01-2038'
-args_argparse_str3 = args_argparse_str2 + ' sort:score'
-args_argparse_str4 = args_argparse_str2_base + ' sort:score:desc score:40'
-args_argparse_str5 = args_argparse_str2_base + ' -header name2=value2'
-args_argparse_str6 = args_argparse_str5 + ' -header Name1=value3 -header NAME2=value4'
-args_argparse_str7 = args_argparse_str1 + ' favorited_by:25000'
-args_argparse_str8 = args_argparse_str1 + ' -merge_lists'
-args_argparse_str9 = args_argparse_str8 + ' -merge_lists -dump_per_item'
-args_argparse_str10 = args_argparse_str1 + ' pool:33600'
-args_argparse_str11 = args_argparse_str1 + ' pool:33600 favorited_by:25000'
+args_argparse_str02 = args_argparse_str02_base + ' -mindate 31-12-1950 -maxdate 01-01-2038'
+args_argparse_str03 = args_argparse_str02 + ' sort:score'
+args_argparse_str04 = args_argparse_str02_base + ' sort:score:desc score:40'
+args_argparse_str05 = args_argparse_str02_base + ' -header name2=value2'
+args_argparse_str06 = args_argparse_str05 + ' -header Name1=value3 -header NAME2=value4'
+args_argparse_str07 = args_argparse_str01 + ' favorited_by:25000'
+args_argparse_str08 = args_argparse_str01 + ' -merge_lists'
+args_argparse_str09 = args_argparse_str08 + ' -merge_lists -dump_per_item'
+args_argparse_str10 = args_argparse_str01 + ' pool:33600'
+args_argparse_str11 = args_argparse_str10 + ' favorited_by:25000'
 args_tagparse_str1 = (
     'sfw asd ned -nds -proxr sort:id sord:score:asc -rating:explicit score:90 '
     '(t1~t2~t3) (t4~t5) -(t6,t7) -(t8,t9,t10) -(t?1,t*2|t?3|t11,t12*,*t13)'
 )
-item_str1_rx = (
+item_str01_rx = (
     '<post height="1291" score="27" file_url="/images/6898/76dfed93372eb7a373ffe2430379cfb1.jpeg" parent_id="90002" '
     'sample_url="/preview/6898/76dfed93372eb7a373ffe2430379cfb1.jpeg" sample_width="961" sample_height="1291" '
     'preview_url="/thumbnails/6898/thumbnail_76dfed93372eb7a373ffe2430379cfb1.jpg" rating="s" '
@@ -59,14 +59,14 @@ item_str1_rx = (
     'md5="76dfed93372eb7a373ffe2430379cfb1" creator_id="1825071" has_children="false" created_at="Sat May 06 07:33:08 +0200 2023" '
     'status="active" source="Twitter/safe" has_notes="false" has_comments="false" preview_width="111" preview_height="150"/>'
 )
-item_str1_rn = (
+item_str01_rn = (
     '<a href="/post/view/427251" class="thumb shm-thumb shm-thumb-link " data-tags="marnie_(pokemon) pokemon ryumigin" '
     'data-height="1200" data-width="848" data-mime="image/png" data-post-id="427251"><img id="thumb_427251" '
     'title="Marnie_(Pokemon) Pokemon RyumiGin // 848x1200 // 1.2MB // png" '
     'alt="Marnie_(Pokemon) Pokemon RyumiGin // 848x1200 // 1.2MB // png" '
     'height="170" width="120"src="/_thumbs/00c90baef0be3a687f37e70c0a1bb291/thumb.jpg"></a>'
 )
-item_str1_rs = (
+item_str01_rs = (
     '<div style="border-radius: 3px; margin: 0px 10px 15px 10px; overflow: hidden; height: 200px; "><a id="7939303" '
     'href="/index.php?r=posts/view&amp;id=7939303"><img  src="/thumbnails/bf/77/thumbnail_bf771345fb58e7ad19087320dc56d76e.jpg" '
     'title="1boy, 1girls, 3d, i love you, kakegurui, kissing, koikatsu, outside, safe, sfw, valentine&#039;s day" '
@@ -75,18 +75,18 @@ item_str1_rs = (
 
 
 class ArgParseTests(TestCase):
-    def test_argparse1(self) -> None:
+    def test_argparse01(self) -> None:
         # 5 tags and all bools, try to intersect
-        args = args_argparse_str1
+        args = args_argparse_str01
         arglist = prepare_arglist(args.split())
         self.assertIsNotNone(arglist.tags)
         # print(str(arglist.tags))
         self.assertEqual(5, len(arglist.tags))
         print(f'{self._testMethodName} passed')
 
-    def test_argparse2(self) -> None:
+    def test_argparse02(self) -> None:
         # 5 tags, value types check
-        args = args_argparse_str2
+        args = args_argparse_str02
         arglist = prepare_arglist(args.split())
         self.assertIsNotNone(arglist.tags)
         self.assertEqual(5, len(arglist.tags))
@@ -101,7 +101,7 @@ class ArgParseTests(TestCase):
 
 
 class TagParseTests(TestCase):
-    def test_tagparse1(self) -> None:
+    def test_tagparse01(self) -> None:
         args = args_tagparse_str1
         res, tags = parse_tags(args)
         self.assertTrue(res)
@@ -110,45 +110,45 @@ class TagParseTests(TestCase):
 
 
 class LoggerTests(TestCase):
-    def test_log1(self) -> None:
+    def test_log01(self) -> None:
         Logger.init(True, False)
         Logger.log('‴﴾₽ὁﻼé₼☼ἦ﴿‴', True, True)
         print(f'{self._testMethodName} passed')
 
 
 class DownloaderBaseTests(TestCase):
-    def test_item1_rx(self) -> None:
+    def test_item01_rx(self) -> None:
         Logger.init(True, True)
-        args = args_argparse_str1
+        args = args_argparse_str01
         arglist = prepare_arglist(args.split())
         with make_downloader(ProcModule.PROC_RX) as dwn:
             dwn._parse_args(arglist)
-            self.assertEqual('7869261', dwn._extract_id(dwn._local_addr_from_string(item_str1_rx)))
-            self.assertEqual('06-05-2023', dwn._extract_post_date(item_str1_rx))
+            self.assertEqual('7869261', dwn._extract_id(dwn._local_addr_from_string(item_str01_rx)))
+            self.assertEqual('06-05-2023', dwn._extract_post_date(item_str01_rx))
         print(f'{self._testMethodName} passed')
 
-    def test_item1_rn(self) -> None:
+    def test_item01_rn(self) -> None:
         Logger.init(True, True)
-        args = args_argparse_str1
+        args = args_argparse_str01
         arglist = prepare_arglist(args.split())
         with make_downloader(ProcModule.PROC_RN) as dwn:
             dwn._parse_args(arglist)
-            self.assertEqual('427251', dwn._extract_id(item_str1_rn))
+            self.assertEqual('427251', dwn._extract_id(item_str01_rn))
         print(f'{self._testMethodName} passed')
 
-    def test_item1_rs(self) -> None:
+    def test_item01_rs(self) -> None:
         Logger.init(True, True)
-        args = args_argparse_str1
+        args = args_argparse_str01
         arglist = prepare_arglist(args.split())
         with make_downloader(ProcModule.PROC_RS) as dwn:
             dwn._parse_args(arglist)
-            self.assertEqual('7939303', dwn._extract_id(dwn._local_addr_from_string(item_str1_rs)))
-            self.assertEqual(DATE_MIN_DEFAULT, dwn._extract_post_date(item_str1_rs))
+            self.assertEqual('7939303', dwn._extract_id(dwn._local_addr_from_string(item_str01_rs)))
+            self.assertEqual(DATE_MIN_DEFAULT, dwn._extract_post_date(item_str01_rs))
         print(f'{self._testMethodName} passed')
 
-    def test_cmdline1(self) -> None:
+    def test_cmdline01(self) -> None:
         Logger.init(True, True)
-        args = args_argparse_str1
+        args = args_argparse_str01
         arglist = prepare_arglist(args.split())
         with make_downloader(ProcModule.PROC_RX) as dwn:
             dwn._parse_args(arglist)
@@ -168,9 +168,9 @@ class DownloaderBaseTests(TestCase):
             self.assertTrue(dwn.append_info)
         print(f'{self._testMethodName} passed')
 
-    def test_cmdline2(self) -> None:
+    def test_cmdline02(self) -> None:
         Logger.init(True, True)
-        args = args_argparse_str2
+        args = args_argparse_str02
         arglist = prepare_arglist(args.split())
         with make_downloader(ProcModule.PROC_RX) as dwn:
             dwn._parse_args(arglist)
@@ -185,17 +185,17 @@ class DownloaderBaseTests(TestCase):
             self.assertEqual('value2', dwn.add_cookies.get('name2'))
         print(f'{self._testMethodName} passed')
 
-    def test_cmdline3(self) -> None:
+    def test_cmdline03(self) -> None:
         Logger.init(True, True)
-        args = args_argparse_str3
+        args = args_argparse_str03
         arglist = prepare_arglist(args.split())
         with make_downloader(ProcModule.PROC_RX) as dwn:
             self.assertRaises(ThreadInterruptException, dwn._parse_args, arglist)
         print(f'{self._testMethodName} passed')
 
-    def test_cmdline4(self) -> None:
+    def test_cmdline04(self) -> None:
         Logger.init(True, True)
-        args = args_argparse_str4
+        args = args_argparse_str04
         arglist = prepare_arglist(args.split())
         with make_downloader(ProcModule.PROC_RX) as dwn:
             dwn._parse_args(arglist)
@@ -203,18 +203,18 @@ class DownloaderBaseTests(TestCase):
             self.assertEqual(7, dwn.get_tags_count())
         print(f'{self._testMethodName} passed')
 
-    def test_cmdline5(self) -> None:
+    def test_cmdline05(self) -> None:
         Logger.init(True, True)
-        args = args_argparse_str5
+        args = args_argparse_str05
         arglist = prepare_arglist(args.split())
         with make_downloader(ProcModule.PROC_RX) as dwn:
             dwn._parse_args(arglist)
             self.assertEqual(2, len(dwn.add_headers))
         print(f'{self._testMethodName} passed')
 
-    def test_cmdline6(self) -> None:
+    def test_cmdline06(self) -> None:
         Logger.init(True, True)
-        args = args_argparse_str6
+        args = args_argparse_str06
         arglist = prepare_arglist(args.split())
         with make_downloader(ProcModule.PROC_RX) as dwn:
             dwn._parse_args(arglist)
@@ -223,27 +223,27 @@ class DownloaderBaseTests(TestCase):
             self.assertEqual('value4', dwn.add_headers['name2'])
         print(f'{self._testMethodName} passed')
 
-    def test_cmdline7(self) -> None:
+    def test_cmdline07(self) -> None:
         Logger.init(True, True)
-        args = args_argparse_str7
+        args = args_argparse_str07
         arglist = prepare_arglist(args.split())
         with make_downloader(ProcModule.PROC_RX) as dwn:
             dwn._parse_args(arglist, False)
             self.assertEqual(25000, dwn.favorites_search_user)
         print(f'{self._testMethodName} passed')
 
-    def test_cmdline8(self) -> None:
+    def test_cmdline08(self) -> None:
         Logger.init(True, True)
-        args = args_argparse_str8
+        args = args_argparse_str08
         arglist = prepare_arglist(args.split())
         with make_downloader(ProcModule.PROC_RX) as dwn:
             dwn._parse_args(arglist, False)
             self.assertTrue(dwn.merge_lists)
         print(f'{self._testMethodName} passed')
 
-    def test_cmdline9(self) -> None:
+    def test_cmdline09(self) -> None:
         Logger.init(True, True)
-        args = args_argparse_str9
+        args = args_argparse_str09
         self.assertRaises(BaseException, prepare_arglist, args.split())
         print(f'{self._testMethodName} passed')
 
@@ -268,7 +268,7 @@ class DownloaderBaseTests(TestCase):
 # Tests below require actual connection
 
 class ConnTests(TestCase):
-    def test_connect_rx1(self) -> None:
+    def test_connect_rx01(self) -> None:
         if not RUN_CONN_TESTS:
             return
         # connection and downloading for rx is performed using same web address, we are free to use dry run here (-dmode 1)
@@ -283,7 +283,7 @@ class ConnTests(TestCase):
             self.assertEqual(1, dwn.total_count)
         print(f'{self._testMethodName} passed')
 
-    def test_connect_rs1(self) -> None:
+    def test_connect_rs01(self) -> None:
         if not RUN_CONN_TESTS:
             return
         # connection and downloading for rx is performed using same web address, we are free to use dry run here (-dmode 1)
@@ -300,7 +300,7 @@ class ConnTests(TestCase):
 
 
 class ItemFilterTests(TestCase):
-    def test_filter_rx1(self) -> None:
+    def test_filter_rx01(self) -> None:
         if not RUN_CONN_TESTS:
             return
         Logger.init(True, True)
@@ -317,7 +317,7 @@ class ItemFilterTests(TestCase):
 
 
 class RealDownloadTests(TestCase):
-    def test_down_rx1(self) -> None:
+    def test_down_rx01(self) -> None:
         if not RUN_CONN_TESTS:
             return
         # connection and downloading for rx is performed using same web address, we are free to use dry run here (-dmode 1)
@@ -331,7 +331,7 @@ class RealDownloadTests(TestCase):
             self.assertTrue(dwn.processed_count == 1, f'dwn.processed_count {dwn.fail_count:d} == 1')
         print(f'{self._testMethodName} passed')
 
-    def test_down_rx2(self) -> None:
+    def test_down_rx02(self) -> None:
         if not RUN_CONN_TESTS:
             return
         # this test actually performs a download
@@ -349,7 +349,7 @@ class RealDownloadTests(TestCase):
             remove_file(tempfile_path)
         print(f'{self._testMethodName} passed')
 
-    def test_down_rs1(self) -> None:
+    def test_down_rs01(self) -> None:
         if not RUN_CONN_TESTS:
             return
         # this test actually performs a download
@@ -367,7 +367,7 @@ class RealDownloadTests(TestCase):
             remove_file(tempfile_path)
         print(f'{self._testMethodName} passed')
 
-    def test_down_rs2_fav1(self) -> None:
+    def test_down_rs02_fav1(self) -> None:
         if not RUN_CONN_TESTS:
             return
         # this test actually performs a download
