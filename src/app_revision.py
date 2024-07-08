bd306fa871e9f1c50cebfa67f1e36c6a59ccd9b7
trickerer01 <onlysuffering+1@gmail.com>
2024-07-08 18:06:51 +0700

    Rev 589: Update excluded tags regex

--- src/app_revision.py
+++ src/app_revision.py
@@ -9,10 +9,10 @@ Author: trickerer (https://github.com/trickerer, https://github.com/trickerer01)
 APP_NAME = 'Ruxx'
 APP_VER_MAJOR = '1'
 APP_VER_SUB = '3'
-APP_REVISION = '588'
+APP_REVISION = '589'
 APP_IS_BETA = False
 APP_IS_BETA_TEXT = 'b' * APP_IS_BETA
-APP_REV_DATE = '06 Jul 2024'
+APP_REV_DATE = '08 Jul 2024'
 APP_VERSION = f'{APP_VER_MAJOR}.{APP_VER_SUB} r{APP_REVISION}{APP_IS_BETA_TEXT}'
 
 #
