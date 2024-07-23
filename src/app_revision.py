45708dc498d3ac620608f41d6438536f0922db76
trickerer01 <onlysuffering+1@gmail.com>
2024-07-23 18:45:23 +0700

    Rev 593: Update README.md

--- src/app_revision.py
+++ src/app_revision.py
@@ -9,10 +9,10 @@ Author: trickerer (https://github.com/trickerer, https://github.com/trickerer01)
 APP_NAME = 'Ruxx'
 APP_VER_MAJOR = '1'
 APP_VER_SUB = '3'
-APP_REVISION = '592'
+APP_REVISION = '593'
 APP_IS_BETA = False
 APP_IS_BETA_TEXT = 'b' * APP_IS_BETA
-APP_REV_DATE = '20 Jul 2024'
+APP_REV_DATE = '23 Jul 2024'
 APP_VERSION = f'{APP_VER_MAJOR}.{APP_VER_SUB} r{APP_REVISION}{APP_IS_BETA_TEXT}'
 
 #
