c56d37aeea4c71db1f8a0378630d1ba286c2668c
trickerer01 <onlysuffering+1@gmail.com>
2024-08-02 07:05:38 +0700

    Rev 623: Add RP tags list

--- src/app_revision.py
+++ src/app_revision.py
@@ -9,10 +9,10 @@ Author: trickerer (https://github.com/trickerer, https://github.com/trickerer01)
 APP_NAME = 'Ruxx'
 APP_VER_MAJOR = '1'
 APP_VER_SUB = '5'
-APP_REVISION = '622'
+APP_REVISION = '623'
 APP_IS_BETA = False
 APP_IS_BETA_TEXT = 'b' * APP_IS_BETA
-APP_REV_DATE = '01 Aug 2024'
+APP_REV_DATE = '02 Aug 2024'
 APP_VERSION = f'{APP_VER_MAJOR}.{APP_VER_SUB} r{APP_REVISION}{APP_IS_BETA_TEXT}'
 
 #
