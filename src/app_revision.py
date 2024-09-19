1a110771f3b5f6caed376b80603f884c28e77fda
trickerer01 <onlysuffering+1@gmail.com>
2024-09-19 09:41:33 +0700

    Rev 679: Update EN/RN/RZ tag lists

--- src/app_revision.py
+++ src/app_revision.py
@@ -9,10 +9,10 @@ Author: trickerer (https://github.com/trickerer, https://github.com/trickerer01)
 APP_NAME = 'Ruxx'
 APP_VER_MAJOR = '1'
 APP_VER_SUB = '6'
-APP_REVISION = '678'
+APP_REVISION = '679'
 APP_IS_BETA = False
 APP_IS_BETA_TEXT = 'b' * APP_IS_BETA
-APP_REV_DATE = '15 Sep 2024'
+APP_REV_DATE = '19 Sep 2024'
 APP_VERSION = f'{APP_VER_MAJOR}.{APP_VER_SUB} r{APP_REVISION}{APP_IS_BETA_TEXT}'
 
 #
