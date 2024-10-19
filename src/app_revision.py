d8218ea879b5f9e66ed146a809409dbe28a460ee
trickerer01 <onlysuffering+1@gmail.com>
2024-10-19 08:48:54 +0700

    Rev 698: Update excluded tags regex

--- src/app_revision.py
+++ src/app_revision.py
@@ -9,10 +9,10 @@ Author: trickerer (https://github.com/trickerer, https://github.com/trickerer01)
 APP_NAME = 'Ruxx'
 APP_VER_MAJOR = '1'
 APP_VER_SUB = '6'
-APP_REVISION = '697'
+APP_REVISION = '698'
 APP_IS_BETA = False
 APP_IS_BETA_TEXT = 'b' * APP_IS_BETA
-APP_REV_DATE = '18 Oct 2024'
+APP_REV_DATE = '19 Oct 2024'
 APP_VERSION = f'{APP_VER_MAJOR}.{APP_VER_SUB} r{APP_REVISION}{APP_IS_BETA_TEXT}'
 
 #
