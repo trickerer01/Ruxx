7f265e17b35ca51043676b730d69398bd7dcae83
trickerer01 <onlysuffering+1@gmail.com>
2024-12-23 16:54:10 +0700

    Rev 746: Revert wrong change from 37631f90
    Signed-off-by: trickerer01 <onlysuffering+1@gmail.com>

--- src/app_revision.py
+++ src/app_revision.py
@@ -9,7 +9,7 @@ Author: trickerer (https://github.com/trickerer, https://github.com/trickerer01)
 APP_NAME = 'Ruxx'
 APP_VER_MAJOR = '1'
 APP_VER_SUB = '7'
-APP_REVISION = '745'
+APP_REVISION = '746'
 APP_IS_BETA = True
 APP_IS_BETA_TEXT = 'b' * APP_IS_BETA
 APP_REV_DATE = '23 Dec 2024'
