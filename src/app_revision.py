38f008c36374d6e765e0ea17ad17349289cfd54b
trickerer01 <onlysuffering+1@gmail.com>
2025-02-19 10:46:00 +0700

    Rev 754: Update tag lists

--- src/app_revision.py
+++ src/app_revision.py
@@ -9,10 +9,10 @@ Author: trickerer (https://github.com/trickerer, https://github.com/trickerer01)
 APP_NAME = 'Ruxx'
 APP_VER_MAJOR = '1'
 APP_VER_SUB = '7'
-APP_REVISION = '753'
+APP_REVISION = '754'
 APP_IS_BETA = True
 APP_IS_BETA_TEXT = 'b' * APP_IS_BETA
-APP_REV_DATE = '17 Feb 2025'
+APP_REV_DATE = '19 Feb 2025'
 APP_VERSION = f'{APP_VER_MAJOR}.{APP_VER_SUB} r{APP_REVISION}{APP_IS_BETA_TEXT}'
 
 #
