ac0948ac1337689cee91a9fc6b09887874f00120
trickerer01 <onlysuffering+1@gmail.com>
2025-05-24 11:26:29 +0700

    Rev 764: Switch to release mode

--- src/app_revision.py
+++ src/app_revision.py
@@ -9,8 +9,8 @@ Author: trickerer (https://github.com/trickerer, https://github.com/trickerer01)
 APP_NAME = 'Ruxx'
 APP_VER_MAJOR = '1'
 APP_VER_SUB = '7'
-APP_REVISION = '763'
-APP_IS_BETA = True
+APP_REVISION = '764'
+APP_IS_BETA = False
 APP_IS_BETA_TEXT = 'b' * APP_IS_BETA
 APP_REV_DATE = '24 May 2025'
 APP_VERSION = f'{APP_VER_MAJOR}.{APP_VER_SUB} r{APP_REVISION}{APP_IS_BETA_TEXT}'
