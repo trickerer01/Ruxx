1525fd5b977a7a6d5e02f1022f9a38d44c9cfd53
trickerer01 <onlysuffering+1@gmail.com>
2025-10-14 09:49:35 +0700

    Rev 801: Add ruff to pre-commit

--- src/app_revision.py
+++ src/app_revision.py
@@ -9,10 +9,10 @@ Author: trickerer (https://github.com/trickerer, https://github.com/trickerer01)
 APP_NAME = 'Ruxx'
 APP_VER_MAJOR = '1'
 APP_VER_SUB = '7'
-APP_REVISION = '800'
+APP_REVISION = '801'
 APP_IS_BETA = False
 APP_IS_BETA_TEXT = 'b' * APP_IS_BETA
-APP_REV_DATE = '13 Oct 2025'
+APP_REV_DATE = '14 Oct 2025'
 APP_VERSION = f'{APP_VER_MAJOR}.{APP_VER_SUB} r{APP_REVISION}{APP_IS_BETA_TEXT}'
 
 #
