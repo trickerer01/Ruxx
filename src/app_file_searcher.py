c7240792dd50f6dbce6ba4772a57d63dab740d73
trickerer01 <onlysuffering+1@gmail.com>
2025-10-22 12:57:36 +0700

    Rev 821: Optimize deduplication algorithm for performance and fix linting

--- src/app_file_searcher.py
+++ src/app_file_searcher.py
@@ -9,7 +9,7 @@ Author: trickerer (https://github.com/trickerer, https://github.com/trickerer01)
 import os
 from contextlib import ExitStack, suppress
 from threading import current_thread
-from typing import Literal
+from typing import BinaryIO, Literal
 
 from app_defines import KNOWN_EXTENSIONS
 from app_utils import normalize_path
@@ -17,10 +17,9 @@ from app_utils import normalize_path
 
 def find_duplicated_files(dest_dict: dict[str, list[str]], basepath: str, scan_depth: int, keep: Literal['first', 'last']) -> None:
     class DFileInfo:
-        def __init__(self, folder: str, name: str, size: int) -> None:
+        def __init__(self, folder: str, name: str) -> None:
             self.folder: str = normalize_path(folder)
             self.name: str = name
-            self.size: int = size
 
         @property
         def fullpath(self) -> str:
@@ -61,35 +60,36 @@ def find_duplicated_files(dest_dict: dict[str, list[str]], basepath: str, scan_d
             if fsize and fsize not in files_by_size:
                 files_by_size[fsize] = []
             fsbase_folder, fname = os.path.split(filepath)
-            files_by_size[fsize].append(DFileInfo(fsbase_folder, fname, fsize))
-        for fsz in list(files_by_size.keys()).copy():
+            files_by_size[fsize].append(DFileInfo(fsbase_folder, fname))
+        for fsz in list(files_by_size.keys()):
             if fsz in files_by_size and len(files_by_size[fsz]) < 2:
                 del files_by_size[fsz]
 
         for filesize, dinfos in files_by_size.items():
             files_list = sorted(dinfos, key=lambda x: x.name, reverse=True)
             with ExitStack() as ctx:
-                open_files = [ctx.enter_context(open(f.fullpath, 'rb')) for f in files_list]
+                open_files: list[BinaryIO] = [ctx.enter_context(open(f.fullpath, 'rb')) for f in files_list]
                 open_fnames = [f.name for f in open_files]
                 exacts = [open_files]
-                fidx = 0
+                fidx = 0  # container may get extended during iteration
                 while fidx < len(exacts):
+                    exacts_fi: list[BinaryIO] = exacts[fidx]
                     if fidx > 0:
-                        for bf in exacts[fidx]:
+                        for bf in exacts_fi:
                             bf.flush()
                             bf.seek(0)
-                    while exacts[fidx][0].tell() + 1 < filesize and len(exacts[fidx]) > 1:
-                        rbytes = [bf.read(read_buffer_size) for bf in exacts[fidx]]
-                        nexacts = list(filter(None, [exacts[fidx][ri] for ri in range(1, len(rbytes)) if rbytes[ri] != rbytes[0]]))
+                    while exacts_fi[0].tell() + 1 < filesize and len(exacts_fi) > 1:
+                        rbyteslist = [bf.read(read_buffer_size) for bf in exacts_fi]
+                        nexacts = [exacts_fi[ri] for ri in range(1, len(rbyteslist)) if rbyteslist[ri] != rbyteslist[0]]
                         for nex in nexacts:
-                            exacts[fidx].remove(nex)
+                            exacts_fi.remove(nex)
                         if len(nexacts) > 1:
                             exacts.append(nexacts)
-                    if len(exacts[fidx]) > 1:
+                    if len(exacts_fi) > 1:
                         if keep == 'first':
-                            exacts = [sorted(li, key=lambda x: x.name) for li in exacts]
-                        dfinfos = [files_list[open_fnames.index(exacts[fidx][fli].name)] for fli in range(len(exacts[fidx]))]
-                        dest_dict[dfinfos[0].fullpath] = [dfinfo.fullpath for dfinfo in dfinfos[1:]]
+                            exacts_fi.sort(key=lambda x: x.name)
+                        dfinfos = [files_list[open_fnames.index(exacts_fi[fli].name)] for fli in range(len(exacts_fi))]
+                        dest_dict[dfinfos[0].fullpath] = [dfinfos[_].fullpath for _ in range(1, len(dfinfos))]
                     fidx += 1
     finally:
         if hasattr(current_thread(), 'killed'):
