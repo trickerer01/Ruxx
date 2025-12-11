c962bc53b50b2ad32288a59f901648af192a448c
trickerer01 <onlysuffering+1@gmail.com>
2025-12-11 09:08:39 +0700

    Rev 871: Make installable as a module

--- ruxx/vcs/version.py
+++ ruxx/vcs/version.py
@@ -9,10 +9,10 @@ Author: trickerer (https://github.com/trickerer, https://github.com/trickerer01)
 APP_NAME = 'Ruxx'
 APP_VER_MAJOR = '1'
 APP_VER_SUB = '7'
-APP_REVISION = '870'
+APP_REVISION = '871'
 APP_IS_BETA = False
 APP_IS_BETA_TEXT = 'b' * APP_IS_BETA
-APP_REV_DATE = '10 Dec 2025'
+APP_REV_DATE = '11 Dec 2025'
 APP_VERSION = f'{APP_VER_MAJOR}.{APP_VER_SUB} r{APP_REVISION}{APP_IS_BETA_TEXT}'
 
 #
