e341f003106d5b79354ae750c4a87a15b14818cb
trickerer01 <onlysuffering+1@gmail.com>
2026-04-13 12:48:32 +0700

    Rev 906: Unify hooks

--- ruxx/vcs/version.py
+++ ruxx/vcs/version.py
@@ -9,10 +9,10 @@ Author: trickerer (https://github.com/trickerer, https://github.com/trickerer01)
 APP_NAME = 'Ruxx'
 APP_VER_MAJOR = '1'
 APP_VER_SUB = '7'
-APP_REVISION = '905'
+APP_REVISION = '906'
 APP_IS_BETA = False
 APP_IS_BETA_TEXT = 'b' * APP_IS_BETA
-APP_REV_DATE = '07 Apr 2026'
+APP_REV_DATE = '13 Apr 2026'
 APP_VERSION = f'{APP_VER_MAJOR}.{APP_VER_SUB} r{APP_REVISION}{APP_IS_BETA_TEXT}'
 
 #
